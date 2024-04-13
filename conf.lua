f0936c585556dc11716dd2048e2bffddadacd2a0
Matt Kelly <matthew.ryan.kelly@gmail.com>
2024-04-12 21:04:21 -0400

    Push for Eli.

--- conf.lua
+++ conf.lua
@@ -2,11 +2,10 @@
 function love.conf(t)
     require("yue")
     t.console = true
-    t.window.title = "YueLOVE"
-    t.window.borderless = true
+    t.window.title = "YueLOVE Falling Sand"
+    t.window.borderless = false
     t.window.resizable = true
     t.window.width = 640
     t.window.height = 480
     t.modules.physics = false
-    t.modules.joystick = false
 end
